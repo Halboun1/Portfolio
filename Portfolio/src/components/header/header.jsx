532072bdd3b1d73bd766ffad74306fa8888c5924
Boun13a <halbounicd@gmail.com>
2022-09-03 17:47:33 -0400

    Working on the Header section

--- Portfolio/src/components/header/header.jsx
+++ Portfolio/src/components/header/header.jsx
@@ -1,14 +1,16 @@
 import React from 'react'
 import './header.css'
+import CTA from './CTA'
 
 const Header = () => {
   return (
     <header>
-        <div className="container header_container">
-            <h5>Hello I'm</h5>
-            <h1>Mohamed Halbouni</h1>
-            <h5 className="text-light">Fullstack Developer</h5>
-        </div>
+      <div className="container header__container">
+        <h5>Hello I'm</h5>
+        <h1>Mohamed Halbouni</h1>
+        <h5 className="text-light">Fullstack Developer</h5>
+        <CTA />
+      </div>
     </header>
   )
 }

c43f75010826131fa8dcb6199cbeebc7e7da9349
Boun13a <halbounicd@gmail.com>
2022-09-06 18:36:27 -0400

    About section

--- Portfolio/src/components/nav/nav.jsx
+++ Portfolio/src/components/nav/nav.jsx
@@ -13,9 +13,9 @@ const Nav = () => {
     <nav>
       <a href='#' onClick={() => setActiveNav('#')} className={activeNav === '#' ? 'active' : ''}><AiOutlineHome/></a>
       <a href='#about' onClick={() => setActiveNav('#about')} className={activeNav === '#about' ? 'active' : ''}><AiOutlineUser/></a>
-      <a href='#experience'><BsBook/></a>
-      <a href='#portfolio'><RiServiceLine/></a>
-      <a href='#contact'><BiMessageDetail/></a>
+      <a href='#experience' onClick={() => setActiveNav('#experience')} className={activeNav === '#experience' ? 'active' : ''}><BsBook/></a>
+      <a href='#portfolio' onClick={() => setActiveNav('#portfolio')} className={activeNav === '#portfolio' ? 'active' : ''}><RiServiceLine/></a>
+      <a href='#contact' onClick={() => setActiveNav('#contact')} className={activeNav === '#contact' ? 'active' : ''}><BiMessageDetail/></a>
     </nav>
   )
 }

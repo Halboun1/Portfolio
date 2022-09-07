7893e99f2d3a1bc6e3b53f37fb41fd1091f05f5d
Boun13a <halbounicd@gmail.com>
2022-09-07 17:29:54 -0400

    Found a bug with the react icon imported

--- Portfolio/src/components/experience/experience.jsx
+++ Portfolio/src/components/experience/experience.jsx
@@ -1,9 +1,32 @@
 import React from 'react'
 import './experience.css'
+import {IoCheckmarkCircleSharp} from 'react-icons/lo'
+
+
+
+
 
 const Experience = () => {
   return (
-    <section id="experience">experience</section>
+    <section id="experience">
+      <h5>Skills Learned</h5>
+      <h2>My Experience</h2>
+
+      <div className="container experience__container">
+        <div className="experience__frontend">
+          <h3>Frontend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <IoCheckmarkCircleSharp/>
+            </article>
+          </div>
+        </div>
+        <div className="experience__backend">
+          
+
+        </div>
+      </div>
+      </section>
   )
 }
 

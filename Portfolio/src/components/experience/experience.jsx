c5858e5d1d3432a95f47ca0b818cf585627199fa
Boun13a <halbounicd@gmail.com>
2022-09-08 16:04:52 -0400

    Experience section is done

--- Portfolio/src/components/experience/experience.jsx
+++ Portfolio/src/components/experience/experience.jsx
@@ -18,11 +18,89 @@ const Experience = () => {
           <div className="experience__content">
             <article className="experience__details">
               <HiBadgeCheck/>
+              <div>
+                <h4>HTML</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>CSS</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>JavaScript</h4>
+                <small className='text-light'>Experienced</small>
+              </div>  
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>Bootstrap</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>             
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>React</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>             
             </article>
           </div>
         </div>
+
         <div className="experience__backend">
-          
+        <h3>Backend Development</h3>
+          <div className="experience__content">
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>Node js</h4>
+                <small className='text-light'>Experienced</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>MongoDB</h4>
+                <small className='text-light'>Bsic</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>PHP</h4>
+                <small className='text-light'>Bsic</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>MySQL</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+
+            <article className="experience__details">
+              <HiBadgeCheck/>
+              <div>
+                <h4>Python</h4>
+                <small className='text-light'>Intermediate</small>
+              </div>
+            </article>
+          </div>
 
         </div>
       </div>

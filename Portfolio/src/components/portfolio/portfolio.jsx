64f1fc28a1ced991d7adc2968e89ffbcbc556435
Boun13a <halbounicd@gmail.com>
2022-09-12 15:18:06 -0400

    creating the portfolio section

--- Portfolio/src/components/portfolio/portfolio.jsx
+++ Portfolio/src/components/portfolio/portfolio.jsx
@@ -3,7 +3,21 @@ import './portfolio.css'
 
 const Portfolio = () => {
   return (
-    <section id="portfolio">portfolio</section>
+    <section id="portfolio">
+      <h5></h5>
+      <h2></h2>
+      <div className="contaienr portfolio__container">
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Halboun1/Handy-App" className='btn'>Github</a>
+          <a href="https://.com" className='btn btn-primary'>Live Demo</a>
+        </article>
+      </div>
+
+    </section>
   )
 }
 

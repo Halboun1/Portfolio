aad540d6485293916329eea23858f29e7f9b3bd4
Boun13a <halbounicd@gmail.com>
2022-09-12 15:36:39 -0400

    Working on portfolio section CSS styling

--- Portfolio/src/components/portfolio/portfolio.jsx
+++ Portfolio/src/components/portfolio/portfolio.jsx
@@ -1,19 +1,48 @@
 import React from 'react'
 import './portfolio.css'
+import IMG from '../../assets/under-construction.jpg'
+
 
 const Portfolio = () => {
   return (
     <section id="portfolio">
-      <h5></h5>
-      <h2></h2>
+      <h5>My Recent Work</h5>
+      <h2>Portfolio</h2>
       <div className="contaienr portfolio__container">
         <article className='portfolio__item'>
           <div className="portfolio__item-image">
-
+            <img src={IMG} alt=""></img>
           </div>
           <h3>This is a portfolio item title</h3>
-          <a href="https://github.com/Halboun1/Handy-App" className='btn'>Github</a>
-          <a href="https://.com" className='btn btn-primary'>Live Demo</a>
+          <a href="https://github.com/Halboun1/Handy-App" target="_blank" rel="noopener noreferrer" className='btn'>Github</a>
+          <a href="https://.com" target="_blank" rel="noopener noreferrer" className='btn btn-primary'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG} alt=""></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Halboun1/Handy-App" target="_blank" rel="noopener noreferrer" className='btn'>Github</a>
+          <a href="https://.com" target="_blank" rel="noopener noreferrer" className='btn btn-primary'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG} alt=""></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Halboun1/Handy-App" target="_blank" rel="noopener noreferrer" className='btn'>Github</a>
+          <a href="https://.com" target="_blank" rel="noopener noreferrer" className='btn btn-primary'>Live Demo</a>
+        </article>
+
+        <article className='portfolio__item'>
+          <div className="portfolio__item-image">
+            <img src={IMG} alt=""></img>
+          </div>
+          <h3>This is a portfolio item title</h3>
+          <a href="https://github.com/Halboun1/Handy-App" target="_blank" rel="noopener noreferrer" className='btn'>Github</a>
+          <a href="https://.com" target="_blank" rel="noopener noreferrer" className='btn btn-primary'>Live Demo</a>
         </article>
       </div>
 
